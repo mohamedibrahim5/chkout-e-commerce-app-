ed991239e9b3eef6e50ae8f5f33628dffd5844d5
mohamedibrahim5 <mohamedibrahimm355@gmail.com>
2022-07-19 12:15:07 +0200

    Adding test SignUo

--- MeritaTests/SignupScreenViewControllertests.swift
+++ MeritaTests/SignupScreenViewControllertests.swift
@@ -9,6 +9,7 @@ import XCTest
 @testable import Merita
 class SignupScreenViewControllertests: XCTestCase {
     let signUp = SignupScreenViewController()
+    let fav = FavouriteScreenViewController()
     
     
 //    override func setUp() async throws {
@@ -17,35 +18,19 @@ class SignupScreenViewControllertests: XCTestCase {
 //    override func tearDown() async throws {
 //        <#code#>
 //    }
-//    
+//
 //    func testcheckEmpty() {
-//        
+//
 //    }
+    func testispass() {
+        let result = signUp.isPasswordValid("moha2")
+        XCTAssert(result == false)
+    }
+    func testStrongpass() {
+        let result = signUp.isPasswordValidMoreComplites("mohamed")
+        XCTAssert(result == false)
+    }
+    
 
 }
 
-
-//
-//func checkEmpty () -> Bool {
-//    if email.text == "" || password.text == "" || name.text == "" {
-//     return true
-//    }else {
-//     return false
-//    }
-//}
-//func weakPassword () -> Bool {
-//  if  password.text!.count < 6 {
-//        return true
-//    }else {
-//        return false
-//    }
-//}
-//func emptyData () {
-//    name.text = ""
-//    email.text = ""
-//    password.text = ""
-//}
-//func isPasswordValid(_ password : String) -> Bool{
-//    let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
-//    return passwordTest.evaluate(with: password)
-//}
